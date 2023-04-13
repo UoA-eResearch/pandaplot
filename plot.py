4d91cdb04102b4e3d883a2b2b4eada649f453cf2
Nick Young <nick.young@auckland.ac.nz>
2023-04-13 17:17:30 +1200

    Read ZONE time from file (removes need to zone_factor), read multiple files and plot them side by side as subplots

--- plot.py
+++ plot.py
@@ -10,9 +10,10 @@ import os
 
 parser = argparse.ArgumentParser(description='Make a graph')
 parser.add_argument('-c', '--colormap', default="jet", help='Which colorscheme to use')
-parser.add_argument('-f', '--file', default="Plot_Data_Elem", help='The file to read in')
+parser.add_argument('-f', '--file', help='The files to read in', nargs='+')
 parser.add_argument('-a', '--axes', default='r,z,P', help='Which columns to plot, in the x y and z dimensions. Comma separated, defaults to r,z,P')
 parser.add_argument('-al', '--axeslabels', help='Labels for the x,y,z axes')
+parser.add_argument('-cl', '--columnlabels', help='Labels for the columns in a subplot')
 parser.add_argument('-s', '--save', action='store_true', help='Whether to save the figure or display it')
 parser.add_argument('-o', '--output_filename', default="output.png", help='The filename to save the resulting figure to')
 parser.add_argument('-d', '--dpi', default=100, type=int, help='DPI of the output')
@@ -22,7 +23,6 @@ parser.add_argument('-e', '--extent', help='Limit the drawn area. Left, right, b
 parser.add_argument('-t', '--ticks', help='Number of ticks', default=5)
 parser.add_argument('-fs', '--font_size', help='Font size', type=int, default=10)
 parser.add_argument('-z', '--zones', help='Which zones to plot')
-parser.add_argument('-zf', '--zone_factor', default=365, type=int, help='Factor to multiply the zone number by')
 parser.add_argument('-sd', '--sample_data', action='store_true', help='Plot some sample data')
 parser.add_argument('-vmin', type=float, help='Override the colorbar min value')
 parser.add_argument('-vmax', type=float, help='Override the colorbar max value')
@@ -59,15 +59,17 @@ def read_file(filename):
     lines = f.readlines()
 
   headers = lines[0].strip().replace("Variables =", "").split()
-  headers.append("zone")
+  headers.extend(["zone", "zone_T"])
   zone = 0
+  zone_time_days = 0
   data = []
   for line in lines[2:]:
     if "ZONE" in line:
+      zone_time_days = int(float(line.split('"')[1]) / 86400)
       zone += 1
     elif line.strip() != "":
       parsed_line = [float(x) for x in line.split()]
-      parsed_line.append(zone)
+      parsed_line.extend([zone, zone_time_days])
       data.append(parsed_line)
   df = pd.DataFrame(data, columns = headers)
   if zones:
@@ -78,58 +80,80 @@ def read_file(filename):
     zones = list(range(zone + 1))
   return df
 
-def plot(df):
-  fig, subplots = plt.subplots(nrows=len(zones), sharex=True, sharey=True, figsize=(10,10), squeeze=False)
-  subplots = subplots.ravel()
+def get_zminmax(dfs):
+  zmins = []
+  zmaxs = []
+  for df in dfs:
+    if extent:
+      x = df[axes[0]]
+      y = df[axes[1]]
+      mask = (x >= extent[0]) & (x <= extent[1]) & (y >= extent[2]) & (y <= extent[3])
+      masked_df = df[mask]
+      zmin = masked_df[axes[2]].min()
+      zmax = masked_df[axes[2]].max()
+    else:
+      zmin = df[axes[2]].min()
+      zmax = df[axes[2]].max()
+    zmins.append(zmin)
+    zmaxs.append(zmax)
 
-  if extent:
-    x = df[axes[0]]
-    y = df[axes[1]]
-    mask = (x >= extent[0]) & (x <= extent[1]) & (y >= extent[2]) & (y <= extent[3])
-    masked_df = df[mask]
-    zmin = masked_df[axes[2]].min()
-    zmax = masked_df[axes[2]].max()
-  else:
-    zmin = df[axes[2]].min()
-    zmax = df[axes[2]].max()
+  zmin = min(zmins)
+  zmax = max(zmaxs)
 
   if args.vmin:
     zmin = args.vmin
   if args.vmax:
     zmax = args.vmax
+  return zmin, zmax
 
-  ims = []
+def plot(dfs):
+  fig, subplots = plt.subplots(ncols=len(dfs), nrows=len(zones), sharex=True, sharey=True, figsize=(10,10), squeeze=False)
+  zmin, zmax = get_zminmax(dfs)
 
-  for i, z in enumerate(zones):
-    dz = df[df["zone"] == z]
-    piv = pd.pivot_table(dz, values=axes[2], index = axes[1], columns=axes[0])
-    if anim:
-      plt.clf()
-      ax = plt.gca()
-    else:
-      ax = subplots[i]
-    im = ax.pcolormesh(piv.columns, piv.index, piv, cmap=args.colormap, vmin=zmin, vmax=zmax)
-    ax.yaxis.set_major_locator(ticker.LinearLocator(args.ticks))
-    ax.xaxis.set_major_locator(ticker.LinearLocator(args.ticks))
-    if extent:
-      ax.set_xlim(extent[:2])
-      ax.set_ylim(extent[2:])
-    ax.set_xlabel(axeslabels[0], fontsize=args.font_size)
-    ax.set_ylabel(axeslabels[1], fontsize=args.font_size)
-    title = ax.set_title("Day {}".format(z * args.zone_factor), x=1.01, y=.5, rotation=-90, horizontalalignment='left', verticalalignment='center', transform=ax.transAxes, fontsize=args.font_size)
-    rect = plt.Rectangle(
-      (1,1), width=1, height=.05, angle=-90, linewidth=1, transform=ax.transAxes, fill=False, clip_on=False
-    )
-    ax.add_patch(rect)
-    ax.label_outer()
+  if args.columnlabels:
+    columnlabels = args.columnlabels.split(",")
+  else:
+    columnlabels = [os.path.basename(f).replace("Plot_Data_Elem_", "") for f in args.file]
 
-    if anim:
-      cb = fig.colorbar(im, ticks=ticker.LinearLocator(args.ticks), format='%.2e')
-      cb.ax.set_title(axeslabels[2], fontsize=args.font_size)
-      fig.canvas.draw() # draw the canvas, cache the renderer
-      image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
-      image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-      ims.append(image)
+  for ax, col in zip(subplots[0], columnlabels):
+    ax.annotate(col, xy=(0.5, 1), xytext=(0, 5),
+                xycoords='axes fraction', textcoords='offset points',
+                size='large', ha='center', va='baseline')
+
+  for df_i, df in enumerate(dfs):
+    ims = []
+
+    for i, z in enumerate(zones):
+      dz = df[df["zone"] == z]
+      piv = pd.pivot_table(dz, values=axes[2], index = axes[1], columns=axes[0])
+      if anim:
+        plt.clf()
+        ax = plt.gca()
+      else:
+        ax = subplots[i, df_i]
+      im = ax.pcolormesh(piv.columns, piv.index, piv, cmap=args.colormap, vmin=zmin, vmax=zmax)
+      ax.yaxis.set_major_locator(ticker.LinearLocator(args.ticks))
+      ax.xaxis.set_major_locator(ticker.LinearLocator(args.ticks))
+      if extent:
+        ax.set_xlim(extent[:2])
+        ax.set_ylim(extent[2:])
+      ax.set_xlabel(axeslabels[0], fontsize=args.font_size)
+      ax.set_ylabel(axeslabels[1], fontsize=args.font_size)
+      day = dz.zone_T.iloc[0]
+      ax.set_title(f"Day {day}", x=1.01, y=.5, rotation=-90, horizontalalignment='left', verticalalignment='center', transform=ax.transAxes, fontsize=args.font_size)
+      rect = plt.Rectangle(
+        (1,1), width=1, height=.08, angle=-90, linewidth=1, transform=ax.transAxes, fill=False, clip_on=False
+      )
+      ax.add_patch(rect)
+      ax.label_outer()
+
+  if anim:
+    cb = fig.colorbar(im, ticks=ticker.LinearLocator(args.ticks), format='%.2e')
+    cb.ax.set_title(axeslabels[2], fontsize=args.font_size)
+    fig.canvas.draw() # draw the canvas, cache the renderer
+    image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
+    image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    ims.append(image)
 
   if anim:
     import imageio
@@ -173,8 +197,8 @@ elif args.sample_data:
   else:
     plt.show()
 else:
-  df = read_file(args.file)
-  fig = plot(df)
+  dfs = [read_file(f) for f in args.file]
+  fig = plot(dfs)
   if args.save:
     if args.output_filename:
       filename = args.output_filename
